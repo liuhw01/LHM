a24ffc405b54a718785ac8484d73c78501bfa055
Hanwei Liu <67702642+liuhw01@users.noreply.github.com>
2025-06-18 14:32:33 +0800

    Update inference_utils.py

--- engine/pose_estimation/pose_utils/inference_utils.py
+++ engine/pose_estimation/pose_utils/inference_utils.py
@@ -4,6 +4,13 @@ import torch
 from pose_utils.camera import get_focalLength_from_fieldOfView
 
 
+# | 参数名          | 含义                            |
+# | ------------ | ----------------------------- |
+# | `img_size`   | 图像的宽度或高度（假设正方形）               |
+# | `fov`        | 视场角 Field of View（单位：度）       |
+# | `p_x`, `p_y` | 主点坐标（归一化 0\~1），若为 None 默认图像中心 |
+# | `device`     | 返回的相机矩阵所在的计算设备（如 GPU）         |
+# 返回，3*3相机内参
 def get_camera_parameters(img_size, fov=60, p_x=None, p_y=None, device=torch.device("cuda")):
     """Given image size, fov and principal point coordinates, return K the camera parameter matrix"""
     K = torch.eye(3)
